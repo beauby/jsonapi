3e9f66af0d731a3286e67f4111f18cc37e1c0719
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-09-27 01:48:33 +0200

    Add yard doc.

--- renderer/lib/jsonapi/renderer.rb
+++ renderer/lib/jsonapi/renderer.rb
@@ -86,10 +86,32 @@ module JSONAPI
 
   module_function
 
+  # Render a success JSON API document.
+  #
+  # @param [(#jsonapi_id, #jsonapi_type, #jsonapi_related, #as_jsonapi),
+  #         Array<(#jsonapi_id, #jsonapi_type, #jsonapi_related, #as_jsonapi)>,
+  #         nil] resources The primary resource(s) to be rendered.
+  # @param [Hash] options All optional.
+  #   @option [String, Hash{Symbol => Hash}] include Relationships to be
+  #     included.
+  #   @option [Hash{Symbol, Array<Symbol>}] fields List of requested fields
+  #     for some or all of the resource types.
+  #   @option [Hash] meta Non-standard top-level meta information to be
+  #     included.
+  #   @option [Hash] links Top-level links to be included.
+  #   @option [Hash] jsonapi_object JSON API object.
   def render(resources, options = {})
     Renderer.new(resources, options).as_json
   end
 
+  # Render an error JSON API document.
+  #
+  # @param [Array<#jsonapi_id>] errors Errors to be rendered.
+  # @param [Hash] options All optional.
+  #   @option [Hash] meta Non-standard top-level meta information to be
+  #     included.
+  #   @option [Hash] links Top-level links to be included.
+  #   @option [Hash] jsonapi_object JSON API object.
   def render_errors(errors)
     Renderer.new(errors, errors: true).as_json
   end
